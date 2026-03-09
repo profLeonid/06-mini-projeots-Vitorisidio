4819f851436ea738992ba3f2c923de5fe1cc501e
VITOR ISIDIO FEITOSA DA SILVA <25203658@CFP127ALUNO.LOCAL>
2026-03-09 10:53:30 -0300

    atividade 1 concluida

---
+++ 01-teste-aptidao/app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+function avaliar() {
+
+    const tempo = document.getElementById('tempo')
+    const resultado = document.getElementById('resultado')
+
+    removerClasses()
+    if (tempo.value < 14) {
+        resultado.textContent = 'Apto'
+        resultado.classList.add('apto')
+    } else {
+        resultado.textContent = 'Não Apto'
+        resultado.classList.add('nao-apto')
+
+    }
+
+
+}
+
+function removerClasses(){
+    const resultado = document.getElementById('resultado')
+    resultado.classList.remove('apto', 'nao-apto')
+    
+}
