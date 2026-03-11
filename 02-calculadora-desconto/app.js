42f3ddf85451e52b0cea9caf8636565ec109a5c4
VITOR ISIDIO FEITOSA DA SILVA <25203658@CFP127ALUNO.LOCAL>
2026-03-11 11:01:53 -0300

    atividade finalizada

--- 02-calculadora-desconto/app.js
+++ 02-calculadora-desconto/app.js
@@ -6,15 +6,32 @@ function calcularDesconto() {
     const desconto = document.getElementById('desconto')
     const resultado = document.getElementById('resultado')
 
+    removerClasses()
 
     if (preco.value && desconto.value) {
-        let resultadoCalculo = preco.value *(desconto.value/100)
-        let precoFinal = preco.value - resultadoCalculo 
-        resultado.textContent = resultadoCalculo +"\n" + "você economizou: " + precoFinal + " Reais"
+        let resultadoCalculo = preco.value * (desconto.value / 100)
+        let precoFinal = preco.value - resultadoCalculo
+        resultado.textContent = "você economizou: " + resultadoCalculo + " Reais," + " Preço Final: " + precoFinal + " Reais"
 
 
     } else {
-                resultado.textContent = 'PREENCHA O CAMPO VAZIO'
+        resultado.textContent = 'PREENCHA O CAMPO VAZIO'
 
     }
+
+
+    if (desconto.value <= 5) {
+        resultado.classList.add('descontoUm')
+    } else if (desconto.value == 6 || desconto.value <= 10) {
+        resultado.classList.add('descontoDois')
+    } else {
+        resultado.classList.add('descontoTres')
+    }
+}
+
+
+function removerClasses(){
+    const resultado = document.getElementById('resultado')
+    resultado.classList.remove('descontoUm', 'descontoDois', "descontoTres")
+    
 }
