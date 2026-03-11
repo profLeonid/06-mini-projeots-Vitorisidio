988671f48c1068b1201b9907aa20e4f1af7a2ee6
Vitorisidio <vitorisidiosenai@gmail.com>
2026-03-11 19:02:33 -0300

    atividade03 finalizada

--- 03-calculadora-media/app.js
+++ 03-calculadora-media/app.js
@@ -5,11 +5,12 @@ function calcularMedia() {
     const nota3 = document.getElementById('nota3')
     const resultado = document.getElementById('resultado')
 
-    if(nota1.value && nota2.value && nota3.value ){
+    if (nota1.value && nota2.value && nota3.value) {
 
-        let resultadoCalculo = (Number(nota1.value) + Number(nota2.value) + Number(nota3.value) )/3
+        let resultadoCalculo = (Number(nota1.value) + Number(nota2.value) + Number(nota3.value)) / 3
+        return resultadoCalculo
 
-    }else{
+    } else {
         resultado.textContent = 'preencha todos os campos'
     }
 
@@ -20,8 +21,23 @@ function media() {
     const resultado = document.getElementById('resultado')
     const mediaFinal = calcularMedia()
 
-    if(mediaFinal => 7){
-        resultado.textContent = 'Aprovado'
+    removerClasses()
+    if (mediaFinal >= 7) {
+        resultado.textContent = 'Aprovado com a nota: ' + mediaFinal
+        resultado.classList.add('aprovado')
+    } else if (mediaFinal >= 5) {
+        resultado.textContent = 'recuperação com a nota: ' + mediaFinal
+        resultado.classList.add('recuperacao')
+    } else {
+        resultado.textContent = 'reprovado com a nota: ' + mediaFinal
+        resultado.classList.add('reprovado')
+
     }
 
+}
+
+function removerClasses(){
+    const resultado = document.getElementById('resultado')
+    resultado.classList.remove('aprovado', 'recuperacao', 'reprovado')
+    
 }
